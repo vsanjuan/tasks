57f916f0a45c4401e37248b67409ae59aedb8b10
salvador.sanjuan@gmail.com <salvador.sanjuan@gmail.com>
2014-09-26 00:27:54 +0200

    Changes to be committed: 	Ya se puede marcar como hecha una tarea pero no detecta el campo. 	modified:   static/scripts/tasks-controller.js 	modified:   templates/tasks.html

--- static/scripts/tasks-controller.js
+++ static/scripts/tasks-controller.js
@@ -5,6 +5,27 @@ tasksController = function() {
 	}
 	var taskPage;
 	var initialised = false;
+	
+	function taskCountChanged(){
+		var count = $(taskPage).find('#tblTasks tbody tr').length;
+		$('footer').find('#taskCount').text(count);
+	}
+	
+	function clearTask() {
+		$(taskPage).find('form').fromObject({});
+	}
+	
+	function renderTable() {
+		$.each($(taskPage).find('#tblTasks tbody tr') , function(idx, row){
+			var due = Date.parse($(row).find('[datetime]').text());
+			if(due.compareTo(Date.today()) < 0) {
+				$(row).addClass("overdue");
+			} else if(due.compareTo((2).days().fromNow()) <=0) {
+				$(row).addClass("warning");
+			}
+		});
+	}
+	
 	return {
 		init: function(page, callback){
 			if(initialised){
@@ -19,13 +40,11 @@ tasksController = function() {
 			if(!initialised){
 				taskPage = page;
 				$(taskPage).find('[required="required"]').prev('label').append('<span>*</span>').children('span').addClass('required');
-				$(taskPage).find('tbody tr:even').addClass('even');
 				$(taskPage).find('#btnAddTask').click(function(evt){
 					evt.preventDefault();
 					$(taskPage).find('#taskCreation').removeClass('not');
 				});
-				$(taskPage).find('tbody tr').click(function(evt) {
-					console.log("Hightlight")
+				$(taskPage).find('#tblTasks tbody').on('click', 'tr', function(evt) {
 					$(evt.target).closest('td').siblings().andSelf().toggleClass('rowHightlight');
 				});
 				$(taskPage).find('#tblTasks tbody').on('click','.deleteRow', 
@@ -33,6 +52,7 @@ tasksController = function() {
 					storageEngine.delete('task',$(evt.target).data().taskId, 
 					function(){
 						$(evt.target).parents('tr').remove();
+						taskCountChanged();
 					},errorLogger);
 				});
 				$(taskPage).find('#saveTask').click(function(evt){
@@ -43,7 +63,7 @@ tasksController = function() {
 						function() {
 							$(taskPage).find('#tblTasks tbody').empty();
 							tasksController.loadTasks();
-							$(':input').val('');
+							clearTask();
 							$(taskPage).find('#taskCreation').addClass('not');
 						},errorLogger);
 					}
@@ -55,16 +75,34 @@ tasksController = function() {
 					function(task) {
 						$(taskPage).find('form').fromObject(task);
 					}, errorLogger);
-				}
-				);
+				});
+				$(taskPage).find('#tblTasks tbody').on('click', '.completeRow', function(evt){
+					storageEngine.findById('task',$(evt.target).data().taskId, function(task){
+						task.complete = true;
+						storageEngine.save('task', task, function(){
+							tasksController.loadTasks();
+						},errorLogger);
+					},errorLogger);
+				});
+				$(taskPage).find('#clearTask').click(function(evt){
+					evt.preventDefault();
+					clearTask();
+				});
 				initialised = true;
 			}
 		},
 		loadTasks: function() {
+			$(taskPage).find('#tblTasks tbody').empty();
 			storageEngine.findAll('task',function(task){
 				$.each(task, function(index,task){
+					if(!task.complete){
+						task.complete = false;
+					}
 					$('#taskRow').tmpl(task).appendTo($(taskPage).find('#tblTasks tbody'));
 				});
+				taskCountChanged();
+				renderTable();
+				$(taskPage).find('tbody tr:even').addClass('even');
 			},errorLogger);
 		}		
 	}
